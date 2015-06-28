56df09e1c719425f3b3185bb57480a9e193315aa
Jenan Wise <jenan@jenanwise.com>
2015-06-28 00:14:15 -0700

    Upgrade refactor-nrepl.

--- homefiles/lein-profiles.clj
+++ homefiles/lein-profiles.clj
@@ -2,5 +2,5 @@
  {:global-vars {*print-length* 100}
   :dependencies [[org.clojure/tools.nrepl "0.2.10"]]
   :plugins [[lein-ancient "0.6.7"]
-            [refactor-nrepl "1.0.5"]
+            [refactor-nrepl "1.1.0-SNAPSHOT"]
             [cider/cider-nrepl "0.9.1"]]}}
