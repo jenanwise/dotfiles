4d7e62e75d1f9d365ef41d6d9d140186c13fc8e3
Jenan Wise <jenan@stripe.com>
2015-11-08 16:11:15 -0800

    lein-profiles version updates.

--- homefiles/lein-profiles.clj
+++ homefiles/lein-profiles.clj
@@ -1,6 +1,7 @@
 {:user
  {:global-vars {*print-length* 100}
-  :dependencies [[org.clojure/tools.nrepl "0.2.10"]]
+  :dependencies [[alembic "0.3.2"]
+                 [org.clojure/tools.nrepl "0.2.11"]]
   :plugins [[lein-ancient "0.6.7"]
-            [refactor-nrepl "1.1.0-SNAPSHOT"]
-            [cider/cider-nrepl "0.9.1"]]}}
+            [refactor-nrepl "2.0.0-SNAPSHOT"]
+            [cider/cider-nrepl "0.10.0-SNAPSHOT"]]}}
