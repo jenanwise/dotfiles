ca866d58b185de0a0f448182535fd47600818955
Jenan Wise <jenan@jenanwise.com>
2015-11-27 13:11:15 -0800

    Bump tools.nrepl version.

--- homefiles/lein-profiles.clj
+++ homefiles/lein-profiles.clj
@@ -1,7 +1,7 @@
 {:user
  {:global-vars {*print-length* 100}
   :dependencies [[alembic "0.3.2"]
-                 [org.clojure/tools.nrepl "0.2.11"]]
+                 [org.clojure/tools.nrepl "0.2.12"]]
   :plugins [[lein-ancient "0.6.7"]
             [refactor-nrepl "2.0.0-SNAPSHOT"]
             [cider/cider-nrepl "0.10.0-SNAPSHOT"]]}}
