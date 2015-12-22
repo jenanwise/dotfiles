a922c7c36b54c24275861f6dcebde512b9e0f09e
Jenan Wise <jenan@jenanwise.com>
2015-12-21 19:33:34 -0800

    lein profile tweaks.

--- homefiles/lein-profiles.clj
+++ homefiles/lein-profiles.clj
@@ -1,8 +1,6 @@
 {:user
  {:global-vars {*print-length* 100}
   :dependencies [[alembic "0.3.2"]
-                 [acyclic/squiggly-clojure "0.1.4"]
                  [org.clojure/tools.nrepl "0.2.12"]]
-  :plugins [[lein-ancient "0.6.7"]
-            [refactor-nrepl "2.0.0-SNAPSHOT"]
-            [cider/cider-nrepl "0.10.0-SNAPSHOT"]]}}
+  :plugins [[refactor-nrepl "2.0.0-SNAPSHOT"]
+            [cider/cider-nrepl "0.10.0"]]}}
