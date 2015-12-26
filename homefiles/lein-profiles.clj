f73c6179535597b51dc54e5d8c7b4662c81d6589
Jenan Wise <jenan@jenanwise.com>
2015-12-26 11:40:31 -0800

    Bump cider.

--- homefiles/lein-profiles.clj
+++ homefiles/lein-profiles.clj
@@ -3,4 +3,4 @@
   :dependencies [[alembic "0.3.2"]
                  [org.clojure/tools.nrepl "0.2.12"]]
   :plugins [[refactor-nrepl "2.0.0-SNAPSHOT"]
-            [cider/cider-nrepl "0.10.0"]]}}
+            [cider/cider-nrepl "0.11.0-SNAPSHOT"]]}}
