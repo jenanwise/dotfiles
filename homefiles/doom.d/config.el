7b8af900a19fdc4e62a734146ee71a0b3f3014c8
Jenan Wise <jenan@jenanwise.com>
2019-11-06 10:40:13 -0800

    misc

--- homefiles/doom.d/config.el
+++ homefiles/doom.d/config.el
@@ -1,4 +1,17 @@
 ;;; .doom.d/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here
+;; Use , as shortcut for SPC-m
 (setq doom-localleader-key ",")
+
+;; Popup repl to the right in vsplit
+(after! clojure
+  (set-popup-rule! "^\\*cider-repl"
+    :quit nil
+    :side 'right
+    :size 0.5))
+
+;; Better configuration for lisp.
+(after! lispy
+  (define-key lispy-mode-map (kbd "[") 'lispy-open-square)
+  (define-key lispy-mode-map (kbd "]") 'lispy-close-square)
+  (lispy-set-key-theme '(lispy c-digits paredit)))
